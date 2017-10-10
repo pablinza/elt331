2154670d38d3fce9850c7e41e51c9e54406dd126
root <root@tecra8.dgac.com.bo>
2017-10-10 10:52:23 +0000

    Actualizado 10/OCT/17

---
+++ p04_ledpotserial/p04_ledpotserial.ino
@@ -0,0 +1,29 @@
+/*
+ * Practica 4. Manejo del puerto Serial y conversion ADC
+ * Enviar por puerto serie el valor de conversión cada segundo, si el voltaje 
+ * es menor a 1V activar led Rojo, si es mayor a 4V activar azul.
+*/
+const int ledR = 9;
+const int ledA = 11;
+int valoradc;
+void setup()
+{
+  Serial.begin(9600); //Configura el Serial Port 9600bps
+  pinMode(9, OUTPUT); //Salida led Rojo
+  pinMode(11, OUTPUT); //Salida led Azul
+}
+
+void loop()
+{
+  valoradc = analogRead(A0); //Lectura del Pot 10k
+  valoradc = map(valoradc, 0 , 1023, 0, 50); //Mapeo
+  if(valoradc >= 40)  //Si valor mayo a 4V
+    digitalWrite(ledA, HIGH); //Activa led Azul
+  if(valoradc <= 10)  //Si valor menor a 1V
+    digitalWrite(ledR, HIGH); //Activa el led Rojo
+  Serial.print("Valor ADC:"); 
+  Serial.print(valoradc); //Envia la conversion ADC por el puerto
+  Serial.println();
+  delay(1000);  //Retardo de 1segundo
+}
+
